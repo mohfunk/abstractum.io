47c5390c42bc4e04ccc9334c9af8f63b5af4598f
Mohammed <mohfunk@gmail.com>
2018-09-01 20:56:05 -0400

    gh,tw icons

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,38 @@
 <template>
   <div class="about">
+      <img class="icon g" src="@/assets/icons/gh.png">
+      <img class="icon t" src="@/assets/icons/tw.png">
   </div>
 </template>
+
+
+
+
+
+
+
+<style lang="scss">
+img.icon {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    top: 75%;
+    animation: bout 1000ms linear both;
+
+    &.g {
+        left: 48%;
+        margin-left: -10px;
+    }
+    &.i {
+        left: 100%
+    }
+    &.t {
+        left: 52%;
+        margin-left: -10px;
+    }
+    &:hover {
+        animation: bin 1000ms linear both;
+        cursor: pointer;
+    }
+}
+</style>
