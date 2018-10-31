2486e122a265ab3450b83dcc54ce32036b0989d4
Mohammed Fahad <mohfunk@gmail.com>
2018-10-30 23:57:00 -0400

    about updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,13 @@
             <h1>Mohammed Alhaythm</h1>
             <h3>Programmer</h3>
             <h5>I'm still pursuing a Bachelors of Mathematics in Computer
-                Science & Combinatorics at the University of Waterloo; However,
-                I decided to work full-time on Wild Thieves until spring 2019.</h5>
+                Science & Combinatorics at the University of Waterloo; however,
+                i'm currently off school temporarily for reasons beyond my
+                control.</h5>
+            <h5>I have been making video games casually since the age of 11, and
+            profissionally since mid 2018 with monupon, a game development
+            studio i'm co-founding with Nasir Al-otabi. We're currently
+            developing Wild Thieves, a Robbery Action Game, coming 2019.</h5>
             <a href="https://twitter.com/mohfunk"><i class="t fab fa-twitter"/></a>
             <a href="https://github.com/mohfunk"><i class="g fab fa-github"/></a>
             <a href="https://www.youtube.com/channel/UCvbPZD6PxSTlW92TKkmBODQ?view_as=subscriber"><i class="y fab fa-youtube"/></a>
